9b88b462b05858c41e2e9804649f0ebc9e4d8654
Kawe Antônio <kaweantonio@gmail.com>
2019-04-02 17:48:57 -0300

    adiciona leitura de tamanho da placa, número de peças e if/else para determinar tipo de peça

---
+++ leitura_dados.py
@@ -0,0 +1,26 @@
+# VARIÁVEIS GLOBAIS
+# dimensões L, W da placa
+placa_L, placa_W = 0, 0
+# número de peças
+N_pecas = 0
+
+# leitura do arquivo
+with open('teste inicial.BiL') as f:
+  # tamanho da placa
+  linha = f.readline().split()
+  placa_L, placa_W = (int(x) for x in linha)
+
+  # número de peças
+  linha = f.readline()
+  N_pecas = int(linha)
+  r, l = 0, 0
+  for i in range(N_pecas):
+    linha = [int(x) for x in (f.readline().split())]
+  
+    # verifica se a peça é regular ou do tipo-L
+    if linha[0] == 0: # peça do tipo L
+      l += 1
+      pass
+    else: # peça regular
+      r += 1
+      pass
