41ae003e389bf8d94822437dcfc54d82fde521af
kaweantonio <kaweantonio@gmail.com>
2019-06-01 13:16:15 -0300

    change the output info on the console

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import config.common as common
 import read.read_data as read
-import draw.draw_pdf as draw
+import draw.draw_pattern as draw
 
 common.__init__()
 
@@ -8,8 +8,14 @@ file_path='./data/teste inicial.BiL'
 
 if read.read_file(file_path=file_path):
   print('Número de peças: {0}, Número de peças R: {1}, Número de peças L: {2}'.format(common.N_pecas, common.N_pecas_R, common.N_pecas_L))
-  print('Lista de peças R: ', common.lista_pecas_R)
-  print('Lista de peças L: ', common.lista_pecas_L)
-  print('Dimensão da placa: ', common.placa_L, 'x', common.placa_W)
+  print('Lista de peças R: ')
+  for i, peca in enumerate(common.lista_pecas_R):
+    print('Peça R #{} (l: {}, w: {})'.format(i+1, peca.l, peca.w))
+
+  print('\nLista de peças L: ')
+  for i, peca in enumerate(common.lista_pecas_L):
+    print('Peça L #{} (l1: {}, w1: {}, l2: {}, w2:{})'.format(i+1, peca.l1, peca.w1, peca.l2, peca.w2))
+  
+  print('\nDimensão da placa: ', common.placa_L, 'x', common.placa_W)
 
   draw.generate_pdf()
