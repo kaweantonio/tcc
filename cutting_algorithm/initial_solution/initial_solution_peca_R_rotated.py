12f1fd4b50aaa9a91c0d3ff384d0483aef9ed9dc
kaweantonio <kaweantonio@gmail.com>
2019-07-18 18:45:44 -0300

    filter R-pieces to rotated == True

--- cutting_algorithm/initial_solution/initial_solution_peca_R_rotated.py
+++ cutting_algorithm/initial_solution/initial_solution_peca_R_rotated.py
@@ -7,7 +7,9 @@ def initial_solution():
 
   min_lost = plate_area
   piece_index = -1
-  for i, piece in enumerate(common.lista_pecas_R):
+
+  lista_pecas = [peca_R for peca_R in common.lista_pecas_R if peca_R.rotated == True]
+  for i, piece in enumerate(lista_pecas):
     piece_l = piece.w
     piece_w = piece.l
     piece_area = piece_l * piece_w
